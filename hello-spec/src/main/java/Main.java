fdbf5908febfa3195a861edff6b24e6d1b5efbde
André Bauer <a.bauer@ovm-kassel.de>
2018-10-08 07:12:20 +0200

    Add hello-spec.

---
+++ hello-spec/src/main/java/Main.java
@@ -0,0 +1,12 @@
+/*
+ *
+ */
+public class Main {
+  public static String getGreeting(String name) {
+    return String.format("Hello %s.", name);
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Geben Sie ihren Namen ein");
+  }
+}
